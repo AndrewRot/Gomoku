e4738794d4172bbef8642201bcc3cf95151256ae
Andrew Rottier <andrewrottier95@gmail.com>
2017-09-16 20:24:46 -0400

    prelimary implementation, winning works in horizontal and vertical ways

--- src/Gomoku.java
+++ src/Gomoku.java
@@ -26,9 +26,7 @@ public class Gomoku {
     private void initializeBoard(){
 
         System.out.println("     A B C D E F G H I J K L M N O ");
-        
         for(int i = 0; i < gameBoard[0].length; i++){
-
             int row = i+1;
             //spaceing
             if(row < 10)
@@ -42,7 +40,6 @@ public class Gomoku {
             }
             System.out.println("]");
         }
-
     }
 
 
@@ -52,24 +49,131 @@ public class Gomoku {
 
         try{
             while(yourTurn){
-                System.out.println("Your move: ");
+                System.out.print("Your move [format 'col row']: ");
                 String line = scanner.nextLine();   
 
                 //manipulate gameboard with your inserted move
-                //to implement
+                //Maybe going to neeed to store this value or write it to a file later
+                String[] move = line.split(" ");
+                int col = convertToIndex(move[0]);
+                int row = Integer.parseInt(move[1])-1;
+                //convert first input from letter to index. Convert second from string to int, - 1 to account for real index of the board starting at 0, even tho labeled 1
+                gameBoard[row] [col] = 'X';
 
+                printBoard();
 
-                yourTurn = false;
-                getComputerMove();
+                //check for win
+                if(checkWin(row, col))
+                    endOfGame(yourTurn);
+                else{ //keep playing, 
+                    yourTurn = false;
+                    getComputerMove();
+                }
             }
         } catch (Exception exc){
             System.out.println("exception caught in Gomoku:" + exc.getMessage());
-        }
-        
+        } 
+    }
+
+    private void endOfGame(boolean playerTurn){
+        if(playerTurn)
+            System.out.println("You have one the game!");
+        else 
+            System.out.println("You lost to AI!");
     }
 
     public void getComputerMove(){
         //generate AI for computers next move.
+
+        for (int i = 0; i < gameBoard[0].length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if (gameBoard[i][j] == '_') {
+                    //moves[0] = i;
+                    //moves[1] = j;
+                    gameBoard[i][j] = 'O';
+                    printBoard();
+
+                    //check for win
+                    if(checkWin(i, j))
+                        endOfGame(yourTurn);
+                    else{ //keep playing, 
+                        yourTurn = true;
+                        
+                        getUserMove();
+                    }
+                }
+            }
+        }
+        //gets to here, board is full
+    }
+
+
+    //Check to see if the previous move won the game for either player
+    //check the four directions
+    private boolean checkWin(int r, int c){
+
+        String symbol = " ";
+        //check win for what letter?
+        if(yourTurn)
+            symbol = "X";
+        else 
+            symbol = "O";
+
+        if (vertical(r, c, symbol) || horizontal(i, j, symbol) ){//|| forwardSlash(i, j, symbol) ){//|| rDiagonal(i, j, symbol)) {
+            return true;
+        }  
+
+        return false;
+    }
+
+    //**** need to implement diagnols. find simple way to check
+    // check for the / direction
+    /*private boolean forwardSlash(int row, int col, String s){
+        int streak = 0;
+        for (int r = 0, int c = 0; r < row && c < col; r++, c++) {
+            if (s.charAt(0) == gameBoard[r][c]) 
+                streak++;
+            else 
+                streak = 0;
+            if(streak == 5)
+                return true;
+        }
+        return false; 
+    }*/
+    private boolean vertical(int r, int c, String s){
+        int streak = 0;
+        for (int i = 0; i < gameBoard[0].length; i++) {
+            if (s.charAt(0) == gameBoard[i][c]) 
+                streak++;
+            else 
+                streak = 0;
+            if(streak == 5)
+                return true;
+        }
+        return false; 
+    }
+     private boolean horizontal(int r, int c, String s){
+        int streak = 0;
+        for (int i = 0; i < gameBoard.length; i++) {
+            if (s.charAt(0) == gameBoard[r][i]) 
+                streak++;
+            else 
+                streak = 0;
+            if(streak == 5)
+                return true;
+        }
+        return false; 
+    }
+
+
+    //convert character to an index
+    private int convertToIndex(String c){
+        String cols = "ABCDEFGHIJKLMNO";
+        for(int i = 0; i < cols.length(); i++){
+            if(cols.charAt(i) == c.charAt(0))
+                return i;
+        }
+        return 0; //*update this to throw an error if letter not valid.
     }
 
 
@@ -158,4 +262,27 @@ public class Gomoku {
 
 
     }
+
+    //print board
+    private void printBoard(){
+
+        System.out.println("     A B C D E F G H I J K L M N O ");
+        for(int i = 0; i < gameBoard[0].length; i++){
+            int row = i+1;
+            //spaceing
+            if(row < 10)
+                System.out.print(row +"  [ ");
+            else
+                System.out.print(row +" [ ");
+
+            for(int j = 0; j < gameBoard.length; j++){
+                System.out.print(gameBoard[i][j] + " ");
+            }
+            System.out.println("]");
+        }
+        System.out.println(); //throw some pace between prints
+    }
+
+
+
 }
