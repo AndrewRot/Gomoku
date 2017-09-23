0f75ddd0af26577ca1b119fcd645f8dcb9282fbe
Barry <bwolfson@wpi.edu>
2017-09-22 22:37:11 -0400

    initial minimax alpha beta pruning algorithm

--- src/MinimaxABPruner.java
+++ src/MinimaxABPruner.java
@@ -1,7 +1,65 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by bwolfson on 9/22/2017.
  */
 public class MinimaxABPruner {
+
+    double evaluate(Board brd){
+        int oneAway = brd.evalStatus(brd.prevPlayer, 1);
+        int twoAway = brd.evalStatus(brd.prevPlayer, 2);
+        int threeAway = brd.evalStatus(brd.prevPlayer, 3);
+        double score = oneAway * 100.0 + twoAway * 5.0 + threeAway * 1.0;
+        return score;
+    }
+
+    Object[] getBestMove(Board brd, int depth, double alpha, double beta){
+        ArrayList<Move> moveList;
+        Set<Move> moves = new HashSet<Move>();
+        LinkedList<Move> occupied = brd.getPlayerPlaces(brd.nextPlayer);
+        for(int i = 0; i < occupied.size(); i++) {
+            moves.addAll(brd.lookAround(occupied.get(i)));
+        }
+        moves.retainAll(brd.findEmpty());
+        if(moves.isEmpty()){
+            moveList = new ArrayList<Move>(brd.findEmpty());
+        }
+        else{
+            moveList = new ArrayList<Move>(moves);
+        }
+
+        Double bestScore;
+        Object[] temp;
+        Double tempScore;
+        Move bestMove = new Move(-1, -1);
+
+        //eval leaf node
+        if(depth == 0){
+            Object[] o = { evaluate(brd), moveList.get(0)};
+            return o;
+        }
+        bestScore = alpha;
+        while(moveList.size() > 0){
+            Board newBrd = new Board(brd.board, brd.nextPlayer, brd.prevPlayer);
+            Move newMove = moveList.get(0);
+            newBrd.executeMove(newBrd.nextPlayer, newMove);
+            temp = getBestMove(newBrd, depth - 1, -beta, -bestScore);
+            tempScore = -(Double) temp[0];
+            if(tempScore > bestScore){
+                bestScore = tempScore;
+                bestMove = newMove;
+            }
+            if(bestScore > beta){
+                Object[] o = { bestScore, bestMove};
+                return o;
+            }
+            moveList.remove(0);
+        }
+        Object[] o = {bestScore, bestMove};
+        return o;
+    }
 }
